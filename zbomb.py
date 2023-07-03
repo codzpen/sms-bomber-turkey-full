e2c1e7e2272e79d72865b4810328c6b7ff3dc2c4
codzpen <120570390+codzpen@users.noreply.github.com>
2023-07-03 11:44:05 +0300

    Update zbomb.py

--- zbomb.py
+++ zbomb.py
@@ -4,7 +4,7 @@ from os import system
 from requests import get
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-r = get("https://raw.githubusercontent.com/cangvnn/Z-Bomb/main/sms.py").text
+r = get("https://raw.githubusercontent.com/codzpen/sms-bomber-turkey-full/main/sms.py").text
 with open("sms.py", "r", encoding="utf-8") as f:
     read = f.read()
 if read == r:
